453e4c7f4f85d63c4557bc9012c7b9f414bf8010
Jonathan <joniimattje@gmail.com>
2024-12-16 10:44:42 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,73 @@
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
+
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
+
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
+	
+		console.log(respuesta);
+
+		// Si la respuesta es correcta
+		if(respuesta.status === 200){
+			const datos = await respuesta.json();
+			
+			let peliculas = '';
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
+			});
+
+			document.getElementById('peliculas').innerHTML = peliculas;
+
+		} else if(respuesta.status === 401){
+			console.log('Pusiste la llave mal');
+		} else if(respuesta.status === 404){
+			console.log('La pelicula que buscas no existe');
+		} else {
+			console.log('Hubo un error y no sabemos que paso');
+		}
+
+	} catch(error){
+		console.log(error);
+	}
+
+}
+
+cargarPeliculas();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const paginacion = document.querySelector('.paginacion');
+
+    window.addEventListener('scroll', () => {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+        const windowHeight = window.innerHeight;
+        const documentHeight = document.documentElement.scrollHeight;
+
+        // Mostrar el botón cuando estamos a 300 píxeles del final de la página
+        if (windowHeight + scrollTop >= documentHeight - 300) { 
+            paginacion.style.display = 'flex';
+        } else {
+            paginacion.style.display = 'none';
+        }
+    });
+});
+
